defe308c8eb03b613ccbfa46c39bd5395865ddd4
Adam Boniecki <adboniec@microsoft.com>
2023-08-03 16:06:54 +0200

    Stub implementation of ExpressionCompiler
    Creates a vsdconfig file that is incorporated into our vsix and used by
    the debugger.

---
+++ vsintegration/src/FSharp.VS.Debugger/FSharpDebuggerExtension/FSharpExpressionCompiler.fs
@@ -0,0 +1,16 @@
+﻿namespace FSharpDebuggerExtension
+
+open Microsoft.VisualStudio.Debugger.ComponentInterfaces
+
+module ExpressionCompiler =
+
+    type FSharpExpressionCompiler() =
+        interface IDkmClrExpressionCompiler with
+            member this.CompileAssignment(expression, instructionAddress, lValue, error, result) =
+                raise (System.NotImplementedException())
+
+            member this.CompileExpression(expression, instructionAddress, inspectionContext, error, result) =
+                raise (System.NotImplementedException())
+
+            member this.GetClrLocalVariableQuery(inspectionContext, instructionAddress, argumentsOnly) =
+                raise (System.NotImplementedException())
